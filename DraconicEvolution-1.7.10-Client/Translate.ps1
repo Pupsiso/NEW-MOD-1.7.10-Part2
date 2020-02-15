7cf49bad539539388376dffe0f6b21866baa1be2
microwin7 <usa.microwin8@gmail.com>
2020-02-16 01:38:10 +0300

    Tranlate Draconic

---
+++ DraconicEvolution-1.7.10-Client/Translate.ps1
@@ -0,0 +1,2 @@
+curl https://github.com/microwin7/Translate-Mods/raw/master/DraconicEvolution/1.7.10/ru_RU.lang | Select -ExpandProperty Content > 'src\main\resources\assets\draconicevolution\lang\ru_RU.lang'
+curl https://github.com/microwin7/Translate-Mods/raw/master/DraconicEvolution/1.7.10/manual/manual-ru_RU.json | Select -ExpandProperty Content > 'src\main\resources\assets\draconicevolution\manual\manual-ru_RU.json'
